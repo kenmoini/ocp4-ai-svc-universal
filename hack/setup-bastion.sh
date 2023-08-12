121d2bb7b34de3411c574d1918f301d48168a631
Tosin Akinosho <takinosh@redhat.com>
2023-08-12 19:58:31 -0400

    updating setup-bastion.sh

--- hack/setup-bastion.sh
+++ hack/setup-bastion.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Set a default repo name if not provided
-REPO_NAME=${REPO_NAME:-tosin2013/device-edge-demos}
+REPO_NAME=${REPO_NAME:-Red-Hat-SE-RTO/ocp4-ai-svc-universal}
 
 # Ensure Git is installed
 echo "Installing Git..."
 sudo dnf install -yq git
 
 # Check if the repository is already cloned
-if [ -d "$HOME/device-edge-demos" ]; then
-  cd $HOME/device-edge-demos
+if [ -d "$HOME/ocp4-ai-svc-universal" ]; then
+  cd $HOME/ocp4-ai-svc-universal
   git config pull.rebase false
   git pull
 else
   cd $HOME
   echo "Cloning from github.com/${REPO_NAME} ..."
   git clone https://github.com/${REPO_NAME}.git
-  cd $HOME/device-edge-demos
+  cd $HOME/ocp4-ai-svc-universal
 fi
 
 # Install System Packages
